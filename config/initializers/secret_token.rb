b0ce584e2b24617a33007031b8c20ccf511edf1e
jpwetstein <jeep.wetstein@gmail.com>
2014-03-26 12:19:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TavisCiTest::Application.config.secret_key_base = '8f4ed0590a57bbc9f9e1180e429c6d9cc730993d165d9d21391ff5e0a42302ee93c18c70129fabe40e35de2e3ba8a64677191c7a0d2a409dc9efe43054c3b42b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TavisCiTest::Application.config.session_store :cookie_store, key: '_tavis_ci_test_session'
